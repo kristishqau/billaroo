3ea553c3ee78bd17a96ab1849ac6fb3ec9f3628e
Kristi Shqau <shqaukristi4@gmail.com>
2025-09-07 13:50:52 +0200

    fix dto

--- server/DTOs/AuthDTOs.cs
+++ server/DTOs/AuthDTOs.cs
@@ -46,10 +46,6 @@ namespace Server.DTOs
         [Required]
         [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
         public required string NewPassword { get; set; }
-
-        [Required]
-        [Compare("NewPassword", ErrorMessage = "Passwords do not match")]
-        public required string ConfirmPassword { get; set; }
     }
 
     public class VerifyEmailDto
